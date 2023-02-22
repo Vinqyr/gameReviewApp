9ee680d251164ab9c008b0317423ef84dff89f92
Nikita <Nikulinbenz@gmail.com>
2023-02-22 21:25:41 +0600

    add validation

--- src/main/java/edu/project/gamereviewapp/DTO/GameRequestDto.java
+++ src/main/java/edu/project/gamereviewapp/DTO/GameRequestDto.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import edu.project.gamereviewapp.Entity.Review;
 import edu.project.gamereviewapp.Enum.Genre;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,13 +15,18 @@ import java.util.List;
 @Getter
 @Builder
 public class GameRequestDto {
+    @NotNull
     private Long id;
+    @NotBlank
     private String name;
+    @NotBlank
     private String developer;
+    @NotNull
     private Genre genre;
     @JsonSerialize(as = LocalDate.class)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate releaseDate;
+    @NotNull
     private List<Review> reviews;
 
 
--- src/main/java/edu/project/gamereviewapp/DTO/ReviewRequestDto.java
+++ src/main/java/edu/project/gamereviewapp/DTO/ReviewRequestDto.java
@@ -1,15 +1,24 @@
 package edu.project.gamereviewapp.DTO;
 
 import edu.project.gamereviewapp.Entity.Game;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 import java.math.BigDecimal;
 
 @Getter
 public class ReviewRequestDto {
+    @NotNull
     private  Long id;
+    @NotNull
+    @Max(value = 10,message = "It's 10 out of 10 max here")
     private  Double score;
+    @NotBlank
     private  String summary;
+    @NotBlank
     private  String similarGames;
+    @NotNull
     private Game game;
 }
